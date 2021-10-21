6f0b7151cc6f77905efccae27c06a43b9ebcd6bf
bakashigure <bakashigure@hotmail.com>
2021-10-21 22:13:40 +0800

    upload

---
+++ 453. 最小操作次数使数组元素相等.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public int MinMoves(int[] nums)
+    {
+        int min = nums.Min();
+        int cnt = 0;
+        for(int i=0;i<nums.Length;i++)
+        {
+            cnt += nums[i] - min;
+        }
+        return cnt;
+    }
+}
