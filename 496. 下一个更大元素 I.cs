da3868213606967ecbf3ed57e6aa36c270355938
bakashigure <bakashigure@hotmail.com>
2021-10-26 23:43:11 +0800

    upload

--- 496. 下一个更大元素 I.cs
+++ 496. 下一个更大元素 I.cs
@@ -1,4 +1,4 @@
-﻿#if true
+﻿#if false
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +9,7 @@ public class Solution
 {
     public int[] NextGreaterElement(int[] nums1, int[] nums2)
     {
-        Stack<int> s = new Stack<int>();
+        Stack<int> s = new();
         Dictionary<int, int> d = new Dictionary<int, int>();
         int n = nums2.Length;
         for (int i = n - 1; i >= 0; --i)
@@ -23,7 +23,7 @@ public class Solution
 
         int[] res = new int[nums1.Length];
         for (int i = 0; i < nums1.Length; i++)
-            res[i] = d[nums1[1]];
+            res[i] = d[nums1[i]];
         return res;
     }
 }
---
+++ 62. 不同路径 I.cs
@@ -0,0 +1,28 @@
+﻿#if false
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Solution
+{
+    public int UniquePaths(int m, int n)
+    {
+        int[,] path = new int[m,n];
+        path[0, 0] = 1;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i > 0 && j > 0) path[i, j] = path[i - 1, j] + path[i, j - 1];
+                else if (i > 0) path[i, j] = path[i - 1, j];
+                else if (j > 0) path[i, j] = path[i, j - 1];
+            }
+        }
+        return path[m - 1, n - 1];
+    }
+}
+
+#endif
